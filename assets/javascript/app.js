b137447425df958be5328fb6be4d20b18f7a0e27
dbell <greenadvisor@gmail.com>
2019-08-05 22:51:20 -0400

    front end set up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,15 @@
 // giphy api key EZDTGbiqXXiqZl2mKgXw3iiXuNNZtQ3S
 
+var videoGames = ["dota", "smash brothers", "goldeneye", "mario kart", "big buck hunter", 
+                "counterstrike", "diablo", "soul calibur", "warcraft", "fifa", "doom"]
+
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+});
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
